5d320155f3ecea848affabbde0db50c366c18043
fujun-iri <fujun-iri@360.cn>
2016-09-07 14:10:44 +0800

    add to-markdown support

--- inject.js
+++ inject.js
@@ -3,12 +3,22 @@ var protocol = location.protocol;
 protocol = /^https?/.test(protocol) ? protocol : 'http';
 
 var jQueryURL = '//apps.bdimg.com/libs/jquery/1.9.1/jquery.js',
-    cookieURL = '//apps.bdimg.com/libs/jquery.cookie/1.4.1/jquery.cookie.js';
+    cookieURL = '//apps.bdimg.com/libs/jquery.cookie/1.4.1/jquery.cookie.js',
+    toMdURL   = '//cdn.bootcss.com/to-markdown/3.0.1/to-markdown.min.js';
 
 chrome.runtime.onMessage.addListener(function(message) {
     if (message.type === "inject") {
         load('jQuery.js', jQueryURL, message, function() {
             load('jquery.cookie.js', cookieURL, message);
+            load('to-markdown.js', toMdURL, message, function() {
+                var sc = document.createElement('script');
+                sc.innerHTML = `
+                    window.getMarked = function(selector) {
+                        prompt('请手动剪切:', toMarkdown($(selector).html()));
+                    };
+                `;
+                document.body.appendChild(sc);
+            });
         });
     }
 });
